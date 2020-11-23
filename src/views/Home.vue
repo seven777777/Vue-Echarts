ac27410f3cddbce31c95574f02a45c92df5fa7e6
seven <se7en.zhang@foxmail.com>
2020-11-23 13:54:20 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,45 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png">
-        <VueEcharts />
+        <Echarts class="chart-box-wrap" :options="opt" />
+        <Echarts :options="opt" />
     </div>
 </template>
 
 <script>
-import VueEcharts from '@/components/VueEcharts.vue'
+import Echarts from '@/components/Echarts.vue'
 
 export default {
     name: 'Home',
     components: {
-        VueEcharts
+        Echarts
+    },
+    data(){
+        return {
+            opt:{
+                xAxis: {
+                    type: 'category',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [{
+                    data: [820, 932, 901, 934, 1290, 1330, 1320],
+                    type: 'line'
+                }]
+            }
+        }
+    },
+    mounted(){
+        
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.chart-box-wrap{
+    width: 50%;
+    height: 400px;
+}
+</style>
