9a1159b69a9b24dd1125216eff8a2928aa53b12c
seven <se7en.zhang@foxmail.com>
2020-11-23 17:41:49 +0800

    update

--- src/utils/mockEchartsData.js
+++ src/utils/mockEchartsData.js
@@ -1,3 +1,19 @@
 export default {
-    
+    baseChartsObj:{
+        xAxisData:["0-1km", "1-3km", "3-5km", "5-7km", "7-9km", "9km以上"],
+        chartData: [
+            {
+                name: '常住地至楼盘距离占比',
+                unit: '%',
+                value:[12,18,34,26,67,10],
+                color: blueGradientBarColor
+            },
+            {
+                name: '工作地地至楼盘距离占比',
+                unit: '%',
+                value:[16,11,14,36,57,20],
+                color: greenGradientBarColor
+            },
+        ]
+    }
 }
